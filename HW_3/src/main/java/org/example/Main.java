e44f1711fa1ade7814b478709457714dfe3810e4
michail <mihail.chertov@gmail.com>
2023-03-14 09:00:16 +0300

    hw-3

---
+++ HW_3/src/main/java/org/example/Main.java
@@ -0,0 +1,82 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Random;
+import static java.lang.Math.round;
+import static java.util.Collections.max;
+import static java.util.Collections.min;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        for (int i = 0; i < 10; i++) {
+            int val = rnd.nextInt(-100, 100);
+            list1.add(val);
+        }
+        System.out.printf("Первоначальный список %s\n", list1);
+        int max = max(list1);
+        int min = min(list1);
+        int maxItem = list1.get(0);
+        int minItem = list1.get(0);
+        int sumItems = 0;
+        for (int item : list1) {
+            if (item > maxItem) {
+                maxItem = item;
+            }
+            if (item < minItem) {
+                minItem = item;
+            }
+            sumItems += item;
+        }
+        float average = (float)sumItems/list1.size();
+        System.out.printf("Максимальный элемент при помощи max %s\n", max);
+        System.out.printf("Максимальный элемент %s\n", maxItem);
+        System.out.printf("Минимальный элемент при помощи min %s\n", min);
+        System.out.printf("Минимальный элемент %s\n", minItem);
+        System.out.printf("Сумма элементов %s\n", sumItems);
+        System.out.printf("Среднее арифметическое %s\n", average);
+    }
+
+    private static void deleteChetn(ArrayList<Integer> arr) {
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr.get(i) % 2 == 0) {
+                arr.remove(i);
+            }
+        }
+    }
+
+    private static void sortArr(int[] array, int low, int high) {
+        if (array.length == 0) {
+            return;
+        }
+        if (low >= high) {
+            return;
+        }
+        int middle = low + (high - low) / 2;
+        int opora = array[middle];
+        int i = low, j = high;
+        while (i <= j) {
+            while (array[i] < opora) {
+                i++;
+            }
+            while (array[j] > opora) {
+                j--;
+            }
+            if (i <= j) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        if (low < j) {
+            sortArr(array, low, j);
+        }
+        if (high > i) {
+            sortArr(array, i, high);
+        }
+    }
+}
